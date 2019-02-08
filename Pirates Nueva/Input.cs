97e0799cd0e47ec525db8c941fd8d1030f9992c2
JoJoJet <joe102000@gmail.com>
2019-02-08 17:34:37 -0500

    Added the Input.Button class -- fields can only be assigned from the Input class

--- Pirates Nueva/Input.cs
+++ Pirates Nueva/Input.cs
@@ -22,6 +22,34 @@ namespace Pirates_Nueva
             var mouse = Mouse.GetState();
 
             MousePosition = mouse.Position;
+            }
+        }
+
+        private interface IContract
+        {
+            void SetIsPressed(bool newValue);
+            void SetOldIsPressed(bool newValue);
+        }
+
+        /// <summary>
+        /// An object representing the status of a button or key this frame.
+        /// </summary>
+        public class Button : IContract
+        {
+            private bool _isPressed;
+            private bool _oldIsPressed;
+
+            /// <summary> Whether or not this <see cref="Button"/> is being pressed this frame. </summary>
+            public bool IsPressed => _isPressed;
+            /// <summary> Whether or not this is the first frame that this <see cref="Button"/> is being pressed. </summary>
+            public bool IsDown => !_oldIsPressed && _isPressed;
+            /// <summary> Whether or not this is the first frame that this <see cref="Button"/> was released. </summary>
+            public bool IsUp => _oldIsPressed && !_isPressed;
+
+            internal Button() {  }
+
+            void IContract.SetIsPressed(bool newValue) => _isPressed = newValue;
+            void IContract.SetOldIsPressed(bool newValue) => _oldIsPressed = newValue;
         }
     }
 }
