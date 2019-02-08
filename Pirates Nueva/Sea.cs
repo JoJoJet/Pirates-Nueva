dbb0fc668b3da144c96bf129067b51c2e9c69a8e
JoJoJet <joe102000@gmail.com>
2019-02-08 16:04:05 -0500

    Sea now has methods to transform coordinates from Sea space to Screen space

--- Pirates Nueva/Sea.cs
+++ Pirates Nueva/Sea.cs
@@ -32,15 +32,15 @@ namespace Pirates_Nueva
             }
         }
 
+        #region Space Transformation
         /// <summary>
         /// Transform the input <see cref="Point"/> from screen space to a pair of coordinates within this <see cref="Sea"/>.
         /// </summary>
         /// <param name="screenPoint">A <see cref="Point"/> in screen space.</param>
         public Vector2 ScreenPointToSea(Point screenPoint) {
-            var (x, y) = ScreenPointToSea((screenPoint.X, screenPoint.Y));
+            var (x, y) = ScreenPointToSea(screenPoint.X, screenPoint.Y);
             return new Vector2(x, y);
         }
-
         /// <summary>
         /// Transform the input coordinates from screen space to a pair of coordinates within this <see cref="Sea"/>.
         /// </summary>
@@ -51,9 +51,33 @@ namespace Pirates_Nueva
         /// </summary>
         /// <param name="x">The x coordinate local to the screen.</param>
         /// <param name="y">The y coordinate local to the screen.</param>
-        internal(float x, float y) ScreenPointToSea(int x, int y) {
+        internal (float x, float y) ScreenPointToSea(int x, int y) {
             int height = Master.GraphicsDevice.Viewport.Height;
             return ((float)x / Block.Pixels, (float)(height - y) / Block.Pixels);
         }
+
+        /// <summary>
+        /// Transform the input <see cref="Vector2"/> from this <see cref="Sea"/> to <see cref="Point"/> local to the screen.
+        /// </summary>
+        /// <param name="seaPoint">A pair of coordinates within this <see cref="Sea"/>.</param>
+        public Point SeaPointToScreen(Vector2 seaPoint) {
+            var (x, y) = SeaPointToScreen(seaPoint.X, seaPoint.Y);
+            return new Point(x, y);
+        }
+        /// <summary>
+        /// Transform the input coordinates from this <see cref="See"/> to the screen.
+        /// </summary>
+        /// <param name="seaPoint">Coordinates local to this <see cref="Sea"/>.</param>
+        internal (int x, int y) SeaPointToScreen((float x, float y) seaPoint) => SeaPointToScreen(seaPoint.x, seaPoint.y);
+        /// <summary>
+        /// Transform the input coordinates from this <see cref="Sea"/> to the screen.
+        /// </summary>
+        /// <param name="x">The x coordinate local to this <see cref="Sea"/>.</param>
+        /// <param name="y">The y coordinate local to this <see cref="Sea"/>.</param>
+        internal (int x, int y) SeaPointToScreen(float x, float y) {
+            int height = Master.GraphicsDevice.Viewport.Height;
+            return ((int)Math.Round(x *  Block.Pixels), (int)Math.Round(height - y * Block.Pixels));
+        }
+        #endregion
     }
 }
