1386f23e0e49fa97ca9a35ca5869f7a388bba82f
JoJoJet <joe102000@gmail.com>
2019-02-08 21:57:04 -0500

    Input class now keeps track of all keys, and has convenient pointers for some of them

--- Pirates Nueva/Input.cs
+++ Pirates Nueva/Input.cs
@@ -10,14 +10,38 @@ namespace Pirates_Nueva
 {
     public class Input : IUpdatable
     {
+        private readonly Dictionary<Keys, Button> _keys = new Dictionary<Keys, Button>();
+
         public Master Master { get; }
 
         public Point MousePosition { get; private set; }
         public Button MouseLeft { get; private set; } = new Button();
         public Button MouseRight { get; private set; } = new Button();
 
+        public Button WKey => GetKey(Keys.W);
+        public Button AKey => GetKey(Keys.A);
+        public Button SKey => GetKey(Keys.S);
+        public Button DKey => GetKey(Keys.D);
+
+        public Button UpKey => GetKey(Keys.Up);
+        public Button LeftKey => GetKey(Keys.Left);
+        public Button DownKey => GetKey(Keys.Down);
+        public Button RightKey => GetKey(Keys.Right);
+
         internal Input(Master master) {
             Master = master;
+        }
+
+        /// <summary>
+        /// Get the button associated with the specified key.
+        /// </summary>
+        public Button GetKey(Keys which) {
+            if(this._keys.TryGetValue(which, out Button b)) {
+                return b;
+            }
+            else {
+                return new Button();
+            }
         }
 
         void IUpdatable.Update(Master master) {
@@ -27,6 +51,20 @@ namespace Pirates_Nueva
             
             MouseLeft = updateButton(MouseLeft, mouse.LeftButton == ButtonState.Pressed);
             MouseRight = updateButton(MouseRight, mouse.RightButton == ButtonState.Pressed);
+
+            var keyboard = Keyboard.GetState();
+
+            // Enter any never-before-pressed keys into the dictionary of pressed keys.
+            foreach(var key in keyboard.GetPressedKeys()) {
+                if(this._keys.ContainsKey(key) == false)
+                    this._keys[key] = new Button();
+            }
+
+            // For every key that has previously been pressed (or queried), update its current status.
+            var allKeys = this._keys.Keys.ToArray();
+            foreach(var key in allKeys) {
+                this._keys[key] = updateButton(this._keys[key], keyboard.IsKeyDown(key));
+            }
 
             Button updateButton(Button old, bool isPressed) {
                 var newb = new Button();
@@ -45,7 +83,7 @@ namespace Pirates_Nueva
         }
 
         /// <summary>
-        /// An object representing the status of a button or key this frame.
+        /// An object representing the status of a button or key during this frame.
         /// </summary>
         public class Button : IContract
         {
