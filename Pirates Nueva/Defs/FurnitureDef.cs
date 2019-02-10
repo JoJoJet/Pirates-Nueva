7f7705fe92ebc45764744cad3728cbffa87d8d9b
JoJoJet <joe102000@gmail.com>
2019-02-09 23:46:47 -0500

    Added the FurnitureDef class

---
+++ Pirates Nueva/Defs/FurnitureDef.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Pirates_Nueva
+{
+    /// <summary>
+    /// An immutable object containing properties for a <see cref="Furniture"/> of a <see cref="Ship"/>.
+    /// </summary>
+    public class FurnitureDef : Def
+    {
+        /// <summary>
+        /// The name of the Texture to display onscreen for a <see cref="Furniture"/> with this <see cref="Def"/>.
+        /// </summary>
+        public string TextureID { get; protected set; }
+
+        /// <summary>
+        /// Get the <see cref="FurnitureDef"/> with identifier /id/.
+        /// </summary>
+        /// <exception cref="KeyNotFoundException">Thrown if there is no <see cref="FurnitureDef"/> identified by /id/.</exception>
+        /// <exception cref="InvalidCastException">Thrown if the <see cref="Def"/> identified by /id/ is not a <see cref="FurnitureDef"/>.</exception>
+        public static FurnitureDef Get(string id) => Get<FurnitureDef>(id);
+
+        protected override void ReadXml(XmlReader parentReader) {
+            using(XmlReader reader = parentReader.ReadSubtree()) {
+                reader.ReadToDescendant("TextureID");
+                TextureID = reader.ReadElementContentAsString();
+            } 
+        }
+    }
+}
