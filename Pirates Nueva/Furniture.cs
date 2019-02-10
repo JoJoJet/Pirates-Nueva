288e262919ba48e51c5a18192969d8f8ea4082f6
JoJoJet <joe102000@gmail.com>
2019-02-10 00:19:39 -0500

    Added the Furniture class

---
+++ Pirates Nueva/Furniture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Nueva
+{
+    public class Furniture
+    {
+        public Ship Ship { get; }
+
+        public FurnitureDef Def { get; }
+        public string ID => Def.ID;
+    }
+}
