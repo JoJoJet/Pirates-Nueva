f8094721bc957bf664b910c9a6749e202ebd5e1c
JoJoJet <joe102000@gmail.com>
2019-02-10 12:14:11 -0500

    Added more constructors and a property to Furniture class

--- Pirates Nueva/Furniture.cs
+++ Pirates Nueva/Furniture.cs
@@ -8,9 +8,29 @@ namespace Pirates_Nueva
 {
     public class Furniture
     {
-        public Ship Ship { get; }
+        public Ship Ship => Floor.Ship;
 
-        public FurnitureDef Def { get; }
+        public FurnitureDef Def { get; private set; }
         public string ID => Def.ID;
+
+        /// <summary>
+        /// The <see cref="Block"/> that this <see cref="Furniture"/> is resting upon.
+        /// </summary>
+        public Block Floor { get; private set; }
+
+        /// <summary> The X index of this <see cref="Furniture"/>, local to its <see cref="Pirates_Nueva.Ship"/>. </summary>
+        public int X => Floor.X;
+        /// <summary> The Y index of this <see cref="Furniture"/>, local to its <see cref="Pirates_Nueva.Ship"/>. </summary>
+        public int Y => Floor.Y;
+
+        public PointI Index => (X, Y);
+
+        /// <summary>
+        /// Create a <see cref="Furniture"/>, defined by the <see cref="FurnitureDef"/> /def/, and placed on the <see cref="Block"/> /block/.
+        /// </summary>
+        public Furniture(FurnitureDef def, Block floor) {
+            Def = def;
+            Floor = floor;
+        }
     }
 }
