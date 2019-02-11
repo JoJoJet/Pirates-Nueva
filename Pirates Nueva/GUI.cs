1179206231dce13e383b1a826e6fcce883d64b50
JoJoJet <joe102000@gmail.com>
2019-02-10 23:22:05 -0500

    Added some documentation and comments to GUI.cs

--- Pirates Nueva/GUI.cs
+++ Pirates Nueva/GUI.cs
@@ -34,10 +34,11 @@ namespace Pirates_Nueva
         /// <exception cref="InvalidOperationException">Thrown if there is already a floating element identified by /id/.</exception>
         public void AddFloating(string id, IFloating floating) {
             if(_floatingElements.ContainsKey(id) == false) {
-                (floating as IFloatingContract).GUI = this;
-                _floatingElements[id] = floating;
-                ArrangeFloating();
+                (floating as IFloatingContract).GUI = this; // Set the /GUI/ property of /floating/ to be this GUI object.
+                _floatingElements[id] = floating;           // Add /floating/ to the dictionary of floating elements.
+                ArrangeFloating(); // Update the arrangement of floating elements after it has been added.
             }
+            // If there is already a floating element identified by /id/, throw an InvalidOperationException.
             else {
                 throw new InvalidOperationException(
                     $"{nameof(GUI)}.{nameof(AddFloating)}(): There is already a floating element named \"{id}\"!"
@@ -56,22 +57,35 @@ namespace Pirates_Nueva
         /// If it was successful, stuffs that value into /floating/.
         /// </summary>
         public bool TryGetFloating<T>(string id, out T floating) where T : IFloating {
-            if(TryGetFloating(id, out IFloating fl)) {
-                // If there is a floating element identified by /id/ and of type /T/.
-                if(fl is T tf) {
-                    floating = tf;
-                    return true;
-                }
-                // If there is a floating element identifed by /id/, but it is not of type /T/.
-                else {
-                    floating = default;
-                    return false;
-                }
+            // If there is a floating element identified by /id/, and it is of type /T/,
+            // set out parameter /floating/ to be that element, and return true.
+            if(TryGetFloating(id, out IFloating med) && med is T last) {
+                floating = last;
+                return true;
             }
-            // If there is no floating element identified by /id/.
+            // If there is no floating element of type /T/ and identified by /id/, return false;
             else {
                 floating = default;
                 return false;
+            }
+        }
+
+        /// <summary>
+        /// Remove the floating element identifed by /id/, and then return it.
+        /// </summary>
+        /// <exception cref="KeyNotFoundException">Thrown when there is no <see cref="IFloating"/> to remove.</exception>
+        public IFloating RemoveFloating(string id) {
+            // If there is a floating element identified by /id/, remove it and then return it.
+            if(this._floatingElements.ContainsKey(id)) {
+                var floating = this._floatingElements[id]; // Store the current element identifed by /id/.
+                this._floatingElements.Remove(id);         // Remove that element from the dictionary.
+                return floating;                           // Return the stored element.
+            }
+            // If there is no floating element identified by /id/, throw a KeyNotFoundException.
+            else {
+                throw new KeyNotFoundException(
+                    $"{nameof(GUI)}.{nameof(RemoveFloating)}(): There is no {nameof(IFloating)} named \"{id}\" to remove!"
+                    );
             }
         }
         #endregion
@@ -186,8 +200,8 @@ namespace Pirates_Nueva
         void IDrawable.Draw(Master master) {
             // Draw every drawable floating element.
             foreach(IFloating floating in this._floatingElements.Values) {
-                if(floating is IFloatingContract drawable)
-                    drawable.Draw(master);
+                if(floating is IFloatingContract drawable) // If /floating/ implements IFloatingContract,
+                    drawable.Draw(master);                 // Call its Draw() method.
             }
         }
 
@@ -196,11 +210,15 @@ namespace Pirates_Nueva
         /// </summary>
         private interface IFloatingContract
         {
+            /// <summary> The position of this element's left edge. </summary>
             int Left { get; set; }
+            /// <summary> The position of this element's top edge. </summary>
             int Top { get; set; }
 
+            /// <summary> Sets the floating element's reference to the GUI object. </summary>
             GUI GUI { set; }
 
+            /// <summary> Draws this floating element onscreen. </summary>
             void Draw(Master master);
         }
 
@@ -211,13 +229,20 @@ namespace Pirates_Nueva
         /// A GUI element floating against an edge of the screen, not part of any menu.
         /// </summary>
         public interface IFloating {
+            /// <summary>  The edge of the screen that this floating element will hug. </summary>
             Edge Edge { get; }
+            /// <summary> The direction that this floating element will stack towards. </summary>
             Direction StackDirection { get; }
 
+            /// <summary> The width of this floating element, in pixels. </summary>
             int WidthPixels { get; }
+            /// <summary> The height of this floating element, in pixels. </summary>
             int HeightPixels { get; }
         }
 
+        /// <summary>
+        /// Action to invoke when a Button is clicked.
+        /// </summary>
         public delegate void OnClick();
         /// <summary>
         /// A button that floats along the edge of a screen, not tied to any menu.
@@ -226,16 +251,23 @@ namespace Pirates_Nueva
         {
             const int Padding = 4;
 
+            /// <summary> Text to display on this <see cref="FloatingButton"/>. </summary>
             public string Text { get; }
 
+            /// <summary> Action to invoke when this <see cref="FloatingButton"/> is clicked. </summary>
             public OnClick OnClick { get; }
 
+            /// <summary> The edge of the screen that this <see cref="FloatingButton"/> will hug. </summary>
             public Edge Edge { get; }
+            /// <summary> The direction that this <see cref="FloatingButton"/> will stack towards. </summary>
             public Direction StackDirection { get; }
 
+            /// <summary> The width of this <see cref="FloatingButton"/>, in pixels. </summary>
             public int WidthPixels => (int)Font.MeasureString(Text).X + Padding*2;
+            /// <summary> The width of this <see cref="FloatingButton"/>, in pixels. </summary>
             public int HeightPixels => (int)Font.MeasureString(Text).Y + Padding*2;
             
+            /// <summary> The <see cref="Pirates_Nueva.GUI"/> that contains this <see cref="FloatingButton"/>. </summary>
             public GUI GUI { get; private set; }
             #region Hidden Properties
             GUI IFloatingContract.GUI { set => this.GUI = value; }
@@ -262,27 +294,29 @@ namespace Pirates_Nueva
         {
             private string _text;
 
-            /// <summary>
-            /// The string of this <see cref="FloatingText"/>.
-            /// </summary>
+            /// <summary> The string of this <see cref="FloatingText"/>. </summary>
             public string Text {
                 get => this._text;
                 set {
-                    string old = this._text;
-                    this._text = value;
-
-                    // Update the arrangement of floating elements after this property is changed.
-                    if(old != value && GUI != null)
-                        GUI.ArrangeFloating();
+                    string old = this._text; // Store the old value of Text.
+                    this._text = value;      // Set the new value of Text.
+                    
+                    if(old != value && GUI != null) // If the value of Text has changed,
+                        GUI.ArrangeFloating();      // update the arrangement of floating elements in GUI.
                 }
             }
 
+            /// <summary> The edge of the screen that this <see cref="FloatingText"/> will hug. </summary>
             public Edge Edge { get; }
+            /// <summary> The direction that this <see cref="FloatingText"/> will stack towards. </summary>
             public Direction StackDirection { get; }
             
+            /// <summary> The width of this <see cref="FloatingText"/>, in pixels. </summary>
             public int WidthPixels => (int)Font.MeasureString(Text).X;
+            /// <summary> The height of this <see cref="FloatingText"/>, in pixels. </summary>
             public int HeightPixels => (int)Font.MeasureString(Text).Y;
 
+            /// <summary> The <see cref="Pirates_Nueva.GUI"/> object that contains this <see cref="FloatingText"/>. </summary>
             public GUI GUI { get; private set; }
             #region Hidden properties
             GUI IFloatingContract.GUI { set => this.GUI = value; }
