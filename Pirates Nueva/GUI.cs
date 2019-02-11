3b16bf8a913e58cc6b29d35776092c6090b3ad83
JoJoJet <joe102000@gmail.com>
2019-02-11 15:35:55 -0500

    Added the GUI.FloatingMenu class

--- Pirates Nueva/GUI.cs
+++ Pirates Nueva/GUI.cs
@@ -465,6 +465,44 @@ namespace Pirates_Nueva
             int X { get; }
             int Y { get; }
         }
+
+        public enum Corner { TopLeft, TopRight, BottomRight, BottomLeft };
+        /// <summary>
+        /// A floating menu that follows a <see cref="IScreenSpaceTarget"/>
+        /// </summary>
+        public class FloatingMenu : Menu
+        {
+            /// <summary> The point that this <see cref="FloatingMenu"/> should follow. </summary>
+            public IScreenSpaceTarget Target { get; }
+            /// <summary> Which corner of the menu will be aligned with <see cref="Target"/>. </summary>
+            public Corner Corner { get; }
+
+            public FloatingMenu(IScreenSpaceTarget target, Corner corner, params MenuElement[] elements) : base(elements) {
+                Target = target;
+                Corner = corner;
+            }
+
+            protected override void Draw(Master master) {
+                // Find the extents of the menu.
+                var (rightBound, bottomBound) = (0, 0);
+                foreach(MenuElement el in Elements) {
+                    rightBound  = Math.Max(rightBound, el.Left + el.WidthPixels  + Padding);
+                    bottomBound = Math.Max(bottomBound, el.Top + el.HeightPixels + Padding);
+                }
+
+                var offset = (Target.X, Target.Y);
+                if(Corner == Corner.TopRight)
+                    offset = (Target.X - rightBound, Target.Y);
+                else if(Corner == Corner.BottomRight)
+                    offset = (Target.X - rightBound, Target.Y - bottomBound);
+                else if(Corner == Corner.BottomLeft)
+                    offset = (Target.X, Target.Y - bottomBound);
+
+                foreach(MenuElement el in Elements) {
+                    var local = (el.Left, el.Top);
+                    (el as IElementDrawable).Draw(master, local.Left + offset.X, local.Top + offset.Y);
+                }
+            }
         }
     }
 }
