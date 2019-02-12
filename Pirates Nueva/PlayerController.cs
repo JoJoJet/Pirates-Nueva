3468f35c1835a4d5b7eaf76949f6a3a7b99bf4cf
JoJoJet <joe102000@gmail.com>
2019-02-12 18:41:22 -0500

    PlayerController now focuses on clicked objects

--- Pirates Nueva/PlayerController.cs
+++ Pirates Nueva/PlayerController.cs
@@ -18,9 +18,21 @@ namespace Pirates_Nueva
     }
     public class PlayerController : IUpdatable
     {
+        private IFocusable _focused;
+
         public Master Master { get; }
 
         private Sea Sea { get; }
+
+        private IFocusable Focused {
+            get => this._focused;
+            set {
+                if(value != this._focused)          // If the focused object changed,
+                    this._focused?.Unfocus(Master); // call its Unfocus() method.
+
+                this._focused = value;
+            }
+        }
 
         internal PlayerController(Master master, Sea sea) {
             Master = master;
@@ -28,7 +40,13 @@ namespace Pirates_Nueva
         }
 
         void IUpdatable.Update(Master master) {
+            if(master.Input.MouseLeft.IsDown) {
+                var (seaX, seaY) = Sea.ScreenPointToSea(master.Input.MousePosition);
+                var focusable = Sea.GetFocusable((seaX, seaY));  // Get any focusable objects under the mouse.
+                Focused = focusable.FirstOrDefault() ?? Focused; // Set the focus to be the first element of /focusable/
+            }
 
+            Focused.Focus(master);
         }
     }
 }
