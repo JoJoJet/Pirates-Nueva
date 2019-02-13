bd07eb413b493a4f4941dc5c84dcba4388ad8582
JoJoJet <joe102000@gmail.com>
2019-02-12 20:19:03 -0500

    Added the BoundingBox immutable struct

---
+++ Pirates Nueva/BoundingBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Nueva
+{
+    public struct BoundingBox
+    {
+        /// <summary> The left edge of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public float Left { get; }
+        /// <summary> The bottom edge of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public float Bottom { get; }
+        /// <summary> The top edge of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public float Top { get; }
+        /// <summary> The right edge of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public float Right { get; }
+
+        public float Width => Right - Left;
+        public float Height => Top - Bottom;
+
+        /// <summary> The bottom-left corner of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public PointF BottomLeft => (Left, Bottom);
+        /// <summary> The top-right corner of this <see cref="BoundingBox"/>, in <see cref="Sea"/>-space. </summary>
+        public PointF TopRight => (Right, Top);
+
+        public BoundingBox(float left, float bottom, float top, float right) {
+            Left = left;
+            Bottom = bottom;
+            Top = top;
+            Right = right;
+        }
+        public BoundingBox(PointF bottomLeft, PointF topRight) : this(bottomLeft.X, bottomLeft.Y, topRight.Y, topRight.X) { }
+    }
+}
