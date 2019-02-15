f88041b0591f4336ba9053920c385f88170517cb
JoJoJet <joe102000@gmail.com>
2019-02-14 22:18:51 -0500

    Added the Texture class, whose base is just a wrapper for a MonoGame.Texture2D

---
+++ Pirates Nueva/UI/Texture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pirates_Nueva.UI
+{
+    /// <summary>
+    /// A 2D image texture.
+    /// </summary>
+    public class Texture
+    {
+        /// <summary> The width of this <see cref="Texture"/>. </summary>
+        public virtual int Width => Drawable.Width;
+        /// <summary> The height of this <see cref="Texture"/>. </summary>
+        public virtual int Height => Drawable.Height;
+
+        public virtual Texture2D Drawable { get; }
+
+        /// <summary>
+        /// Create a new <see cref="Texture"/> from a MonoGame <see cref="Texture2D"/>.
+        /// </summary>
+        public Texture(Texture2D inner) {
+            Drawable = inner;
+        }
+        /// <summary>
+        /// Create a blank <see cref="Texture2D"/>; only usable from derived classes.
+        /// </summary>
+        protected Texture() { }
+    }
+}
