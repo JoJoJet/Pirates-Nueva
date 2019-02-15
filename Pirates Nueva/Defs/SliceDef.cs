1d0b3f5cdde4ea15855c8fa27682aa68a3f859c2
JoJoJet <joe102000@gmail.com>
2019-02-15 00:21:29 -0500

    Added the SliceDef class

---
+++ Pirates Nueva/Defs/SliceDef.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Pirates_Nueva
+{
+    /// <summary>
+    /// An immutable object containing the definition of a <see cref="UI.NineSlice"/> texture.
+    /// </summary>
+    public class SliceDef : Def
+    {
+        internal string Texture { get; private set; }
+        internal (int left, int right, int top, int bottom) Slices { get; private set; }
+
+        protected override void ReadXml(XmlReader parentReader) {
+            using(var reader = parentReader.ReadSubtree()) {
+                reader.ReadToDescendant("TextureID");
+                Texture = reader.ReadElementContentAsString();
+
+                reader.ReadToNextSibling("Slices");
+
+                reader.ReadToDescendant("Left");
+                int left = reader.ReadElementContentAsInt();
+
+                reader.ReadToNextSibling("Right");
+                int right = reader.ReadElementContentAsInt();
+
+                reader.ReadToNextSibling("Top");
+                int top = reader.ReadElementContentAsInt();
+
+                reader.ReadToNextSibling("Bottom");
+                int bottom = reader.ReadElementContentAsInt();
+
+                this.Slices = (left, right, top, bottom);
+            }
+        }
+    }
+}
