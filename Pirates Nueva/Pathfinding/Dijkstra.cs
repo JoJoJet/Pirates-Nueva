70bf7d16d5d2cd21a80f73bb01cd838645e69199
JoJoJet <joe102000@gmail.com>
2019-02-16 12:27:16 -0500

    Added the Pathfinding.Edge struct; an edge between two nodes

---
+++ Pirates Nueva/Pathfinding/Dijkstra.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Nueva.Pathfinding
+{
+    /// <summary>
+    /// End edge between two <see cref="INode"/>s.
+    /// </summary>
+    public struct Edge
+    {
+        /// <summary> The cost to move along this <see cref="Edge"/>. </summary>
+        public float Cost { get; }
+
+        /// <summary> The node that this <see cref="Edge"/> connects to. </summary>
+        public INode End { get; }
+
+        public Edge(float cost, INode end) {
+            Cost = cost;
+            End = end;
+        }
+    }
+}
