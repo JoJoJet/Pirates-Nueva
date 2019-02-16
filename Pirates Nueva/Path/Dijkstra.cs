a27ea2e4c114c36a4e3110c839776995e04845b5
JoJoJet <joe102000@gmail.com>
2019-02-16 14:28:52 -0500

    Pathfinding is now generic

--- Pirates Nueva/Path/Dijkstra.cs
+++ Pirates Nueva/Path/Dijkstra.cs
@@ -9,31 +9,31 @@ namespace Pirates_Nueva.Path
     /// <summary>
     /// A pathfinding graph.
     /// </summary>
-    public interface IGraph
+    public interface IGraph<T>
     {
         /// <summary> Every <see cref="INode"/> in this pathfinding graph. </summary>
-        IEnumerable<INode> Nodes { get; }
+        IEnumerable<INode<T>> Nodes { get; }
     }
     /// <summary>
     /// Part of a pathfinding graph.
     /// </summary>
-    public interface INode
+    public interface INode<T>
     {
         /// <summary> The edges moving FROM this <see cref="INode"/>. </summary>
-        IEnumerable<Edge> Edges { get; }
+        IEnumerable<Edge<T>> Edges { get; }
     }
     /// <summary>
     /// End edge between two <see cref="INode"/>s.
     /// </summary>
-    public struct Edge
+    public struct Edge<T>
     {
         /// <summary> The squared cost to move along this <see cref="Edge"/>. </summary>
         public float SqrCost { get; }
 
         /// <summary> The node that this <see cref="Edge"/> connects to. </summary>
-        public INode End { get; }
+        public INode<T> End { get; }
 
-        public Edge(float sqrCost, INode end) {
+        public Edge(float sqrCost, INode<T> end) {
             SqrCost = sqrCost;
             End = end;
         }
@@ -45,36 +45,38 @@ namespace Pirates_Nueva.Path
     /// </summary>
     public static class Dijkstra
     {
-        public static Stack<INode> FindPath(IGraph graph, INode source, INode target) {
-            var dist = new Dictionary<INode, float>(); // The squared distance from /source/ for each node.
-            var prev = new Dictionary<INode, INode>(); // The node before the key in the optimal path to /source/.
-            var Q = new List<INode>();                 // Set of unvisited nodes.
+        /// <summary>
+        /// Find the shortest distance between nodes /source/ and /target/ on the specified graph.
+        /// </summary>
+        public static Stack<T> FindPath<T>(IGraph<T> graph, INode<T> source, INode<T> target) where T : INode<T> {
+            var dist = new Dictionary<INode<T>, float>();    // The squared distance from /source/ for each node.
+            var prev = new Dictionary<INode<T>, INode<T>>(); // The node before the key in the optimal path to /source/.
+            var Q = new List<INode<T>>();                    // Set of unvisited nodes.
 
-            foreach(var v in graph.Nodes) {
-                dist[v] = float.MaxValue;              // Unkown distance from /source/ to /v/.
-                Q.Add(v);                              // Add /v/ to the list of unvisited nodes
+            foreach(var v in graph.Nodes) { // For every node in the graph:
+                dist[v] = float.MaxValue;   // Unkown distance from /source/ to the node.
+                Q.Add(v);                   // Add it to the list of unvisited nodes
             }
+            dist[source] = 0;               // Distance from /source/ to itself is always 0;
 
-            dist[source] = 0;                          // Distance from /source/ to itself is always 0;
-
-            while(Q.Count > 0) {                       // While there are still unvisited nodes:
-                var u = (from n in Q                   // Node with least distance from /source/.
+            while(Q.Count > 0) {                        // While there are still unvisited nodes:
+                var u = (from n in Q                    // Get the node with lowest distance from /source/.
                          orderby dist[n] ascending
                          select n).First();
 
-                Q.Remove(u);                           // Remove /u/ from /Q/.
+                Q.Remove(u);                            // Remove the node from the unvisited nodes.
 
-                if(u == target)                        // If we are at /target/, we can stop constructing the tree.
+                if(u == target)                         // If we are at /target/, we can stop looping.
                     break;
 
-                var neighbors = from n in u.Edges      // Every neighbor of /u/ that is unvisited.
+                var neighbors = from n in u.Edges       // Every neighbor of /u/ that is unvisited.
                                 where Q.Contains(n.End)
                                 select n;
-                foreach(var v in neighbors) {          // For every unvisited neighbor:
-                    var alt = dist[u] + v.SqrCost;     //     The distance from /source/ to /v/, going through /u/.
-                    if(alt < dist[v.End]) {            //     If the above distance is a shorter than dist[v], we found a shorter path.
-                        dist[v.End] = alt;             //         update dist[v]
-                        prev[v.End] = u;               //         set a new previous node for /v/.
+                foreach(var v in neighbors) {           // For every unvisited neighbor:
+                    var alt = dist[u] + v.SqrCost;      //     The distance from /source/ to /v/, going through /u/.
+                    if(alt < dist[v.End]) {             //     If the above distance is a shorter than dist[v], we found a shorter path.
+                        dist[v.End] = alt;              //         update dist[v]
+                        prev[v.End] = u;                //         set a new previous node for /v/.
                     }
                 }
             }
@@ -82,12 +84,12 @@ namespace Pirates_Nueva.Path
             return reverse_iterate();                  // Construct the optimal path by reverse
                                                        // iterating through /prev/ from /target/.
             
-            Stack<INode> reverse_iterate() {
-                var S = new Stack<INode>();                       // Empty sequence.
+            Stack<T> reverse_iterate() {
+                var S = new Stack<T>();                       // Empty sequence.
                 var u = target;
                 if(prev.ContainsKey(u) || u == source) {          // If the vertex is reachable:
                     while(u != null) {                            // Construct the shortest path
-                        S.Push(u);                                //     push the vertex onto the stack
+                        S.Push((T)u);                                //     push the vertex onto the stack
                         u = prev.ContainsKey(u) ? prev[u] : null; //     set /u/ as the previous node in the optimal path.
                     }                                             //
                     return S;                                     // Return the path we have constructed.
