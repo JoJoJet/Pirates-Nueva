36c13b4cdd14d40b59db571dfb95439c455364af
JoJoJet <joe102000@gmail.com>
2019-02-16 15:03:58 -0500

    The player can now set an Agent's path

--- Pirates Nueva/Ships/Agent.cs
+++ Pirates Nueva/Ships/Agent.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pirates_Nueva.Path;
 
 namespace Pirates_Nueva
 {
     public class Agent : IDrawable, IFocusable, UI.IScreenSpaceTarget
     {
+        private Stack<Block> _path;
+
         /// <summary> The <see cref="Pirates_Nueva.Ship"/> that contains this <see cref="Agent"/>. </summary>
         public Ship Ship { get; }
 
@@ -27,6 +30,11 @@ namespace Pirates_Nueva
 
         /// <summary> Linearly interpolate between two values, by amount /f/. </summary>
         private float Lerp(float a, float b, float f) => a * (1 - f) + b * f;
+
+        protected Stack<Block> Path {
+            get => this._path;
+            set => this._path = (value.Count > 0 ? value : null);
+        }
 
         public Agent(Ship ship, Block floor) {
             Ship = ship;
@@ -72,16 +80,17 @@ namespace Pirates_Nueva
             if(this.focusMode == FocusOption.ChoosePath) { // If we are currently choosing a path,
                 IsFocusLocked = true;                      //     lock the focus onto this agent.
 
-                if(master.Input.MouseLeft.IsDown && !master.GUI.IsMouseOverGUI) {             // If the user clicked, but not on GUI,
+                if(master.Input.MouseLeft.IsDown && !master.GUI.IsMouseOverGUI) {             // If the user clicked, but not on GUI:
                     var (seaX, seaY) = Ship.Sea.ScreenPointToSea(master.Input.MousePosition);
-                    var (shipX, shipY) = Ship.SeaPointToShip(seaX, seaY);
-
-                    if(shipX >= 0 && shipX < Ship.Width && shipY >= 0 && shipY < Ship.Height) {
-                        // TODO: Make the agent path to /shipX/, /shipY/.
+                    var (shipX, shipY) = Ship.SeaPointToShip(seaX, seaY);               // The point the user clicked,
+                                                                                        //     local to the ship.
+                    if(Ship.AreIndicesValid(shipX, shipY) &&                            // If the spot is a valid index,
+                        Ship.GetBlock(shipX, shipY) is Block target) {                  // and it has a block,
+                        Path = Dijkstra.FindPath(Ship, CurrentBlock, target); //     have the agent path to that block.
                     }
 
-                    this.focusMode = FocusOption.None; //     unset the focus mode,
-                    IsFocusLocked = false;             //     and release the focus.
+                    this.focusMode = FocusOption.None; // Unset the focus mode,
+                    IsFocusLocked = false;             // and release the focus.
                 }
             }
         }
