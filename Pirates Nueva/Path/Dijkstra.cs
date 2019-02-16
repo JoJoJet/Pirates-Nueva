61dffdcf3a5717607f8cf8d7e59faa24a55035af
JoJoJet <joe102000@gmail.com>
2019-02-16 15:17:14 -0500

    Moved data structures from the Path namespace into their own file

--- Pirates Nueva/Path/Dijkstra.cs
+++ Pirates Nueva/Path/Dijkstra.cs
@@ -7,38 +7,6 @@ using System.Threading.Tasks;
 namespace Pirates_Nueva.Path
 {
     /// <summary>
-    /// A pathfinding graph.
-    /// </summary>
-    public interface IGraph<T>
-    {
-        /// <summary> Every <see cref="INode"/> in this pathfinding graph. </summary>
-        IEnumerable<INode<T>> Nodes { get; }
-    }
-    /// <summary>
-    /// Part of a pathfinding graph.
-    /// </summary>
-    public interface INode<T>
-    {
-        /// <summary> The edges moving FROM this <see cref="INode"/>. </summary>
-        IEnumerable<Edge<T>> Edges { get; }
-    }
-    /// <summary>
-    /// End edge between two <see cref="INode"/>s.
-    /// </summary>
-    public struct Edge<T>
-    {
-        /// <summary> The squared cost to move along this <see cref="Edge"/>. </summary>
-        public float SqrCost { get; }
-
-        /// <summary> The node that this <see cref="Edge"/> connects to. </summary>
-        public INode<T> End { get; }
-
-        public Edge(float sqrCost, INode<T> end) {
-            SqrCost = sqrCost;
-            End = end;
-        }
-    }
-    /// <summary>
     /// Pathfinding with Dijkstra'a algorithm.
     /// <para />
     /// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm.
---
+++ Pirates Nueva/Path/Structures.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Pirates_Nueva.Path
+{
+    /// <summary>
+    /// A pathfinding graph.
+    /// </summary>
+    public interface IGraph<T>
+    {
+        /// <summary> Every <see cref="INode"/> in this pathfinding graph. </summary>
+        IEnumerable<INode<T>> Nodes { get; }
+    }
+    /// <summary>
+    /// Part of a pathfinding graph.
+    /// </summary>
+    public interface INode<T>
+    {
+        /// <summary> The edges moving FROM this <see cref="INode"/>. </summary>
+        IEnumerable<Edge<T>> Edges { get; }
+    }
+    /// <summary>
+    /// End edge between two <see cref="INode"/>s.
+    /// </summary>
+    public struct Edge<T>
+    {
+        /// <summary> The squared cost to move along this <see cref="Edge"/>. </summary>
+        public float SqrCost { get; }
+
+        /// <summary> The node that this <see cref="Edge"/> connects to. </summary>
+        public INode<T> End { get; }
+
+        public Edge(float sqrCost, INode<T> end) {
+            SqrCost = sqrCost;
+            End = end;
+        }
+    }
+}
