aec88ed35721e962401a7c49aa38ff71188e878f
JoJoJet <joe102000@gmail.com>
2019-02-16 01:10:17 -0500

    Added the Agent class

---
+++ Pirates Nueva/Ships/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Nueva
+{
+    public class Agent
+    {
+        /// <summary> The <see cref="Pirates_Nueva.Ship"/> that contains this <see cref="Agent"/>. </summary>
+        public Ship Ship { get; }
+
+        /// <summary> The X index of this <see cref="Agent"/>, local to its <see cref="Pirates_Nueva.Ship"/>. </summary>
+        public int X { get; protected set; }
+        /// <summary> The Y index of this <see cref="Agent"/>, local to its <see cref="Pirates_Nueva.Ship"/>. </summary>
+        public int Y { get; protected set; }
+
+        public Agent(Ship ship, int x, int y) {
+            Ship = ship;
+            X = x;
+            Y = y;
+        }
+    }
+}
