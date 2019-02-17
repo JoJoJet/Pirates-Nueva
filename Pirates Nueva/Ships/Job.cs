43e1bc0eef25a5fe7050b52f0af45b678f071cab
JoJoJet <joe102000@gmail.com>
2019-02-17 15:13:24 -0500

    Toils now have a Requirement and an Action

--- Pirates Nueva/Ships/Job.cs
+++ Pirates Nueva/Ships/Job.cs
@@ -26,6 +26,27 @@ namespace Pirates_Nueva
         /// </summary>
         public class Toil
         {
+            public Requirement Requirement { get; }
+            public Action Action { get; }
+            
+            public Toil(Requirement req, Action action) {
+                Requirement = req;
+                Action = action;
+            }
+        }
+        /// <summary>
+        /// Something that must be fulfilled before a <see cref="Toil"/> can be completed.
+        /// </summary>
+        public abstract class Requirement
+        {
+            public abstract bool Qualify(Agent worker);
+        }
+        /// <summary>
+        /// What a <see cref="Toil"/> will do after its <see cref="Requirement"/> is fulfilled.
+        /// </summary>
+        public abstract class Action
+        {
+            public abstract bool IsCompleted { get; }
         }
     }
 }
