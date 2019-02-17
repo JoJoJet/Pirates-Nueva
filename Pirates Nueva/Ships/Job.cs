c5909a20199652d77c563d791225b8db46091a8c
JoJoJet <joe102000@gmail.com>
2019-02-17 15:06:55 -0500

    Added the Job class

---
+++ Pirates Nueva/Ships/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Nueva
+{
+    /// <summary>
+    /// Something that an <see cref="Agent"/> can do in a <see cref="Ship"/>.
+    /// </summary>
+    public class Job
+    {
+    }
+}
