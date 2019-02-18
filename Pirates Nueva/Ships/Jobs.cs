f92523406f5e001d56b3280d933cb093821d320a
JoJoJet <joe102000@gmail.com>
2019-02-17 19:18:27 -0500

    Added the PathToAdjacent job action

--- Pirates Nueva/Ships/Jobs.cs
+++ Pirates Nueva/Ships/Jobs.cs
@@ -12,13 +12,13 @@ namespace Pirates_Nueva
         /// Check if the specified <see cref="Agent"/> is adjacent to the <see cref="Job.Toil"/>.
         /// </summary>
         protected override bool Qualify(Agent worker, out string reason) {
-            if(PointF.SqrDistance((worker.X, worker.Y), (Toil.X, Toil.Y)) == 1f) {
-                reason = "";
-                return true;
-            }
-            else {
-                reason = "Worker is not adjacent to the job.";
-                return false;
+            if(PointF.SqrDistance((worker.X, worker.Y), Toil.Index) == 1f) { // If the worker is adjacent to the toil,
+                reason = "";                                                 //     set the reason as an empty string,
+                return true;                                                 //     and return true.
+            }                                                                //
+            else {                                                           // If the worker is NOT adjacent to the toil,
+                reason = "Worker is not adjacent to the job.";               //     set that as the reason,
+                return false;                                                //     and return false.
             }
         }
     }
@@ -29,8 +29,38 @@ namespace Pirates_Nueva
         /// </summary>
         /// <returns>Whether or not the action was just completed.</returns>
         protected override bool Work(Agent worker) {
-            Toil.Ship.PlaceBlock("wood", Toil.X, Toil.Y);
+            Toil.Ship.PlaceBlock("wood", Toil.X, Toil.Y); // Place a block at the toil's position,
+            return true;                                  // and return true.
+        }
+    }
+
+    public class AlwaysTrue : Job.Requirement
+    {
+        protected override bool Qualify(Agent worker, out string reason) {
+            reason = "";
             return true;
+        }
+    }
+
+    public class PathToAdjacent : Job.Action
+    {
+        protected override bool Work(Agent worker) {
+            if(worker.PathingTo == null) {                // If the worker is currently still:
+                if(isAdjacent(worker.CurrentBlock)) {     //     If its standing next to the toil,
+                    return true;                          //         return true.
+                }                                         //
+                else {                                    //     If its standing away from the toil,
+                    worker.PathTo(isAdjacent);            //         have it path to a spot adjacent to the toil,
+                    return false;                         //         and return false.
+                }                                         //
+            }                                             //
+            else {                                        // If the worker is currently pathing:
+                if(isAdjacent(worker.PathingTo) == false) //     if the worker's destination is not adjacent to the toil,
+                    worker.PathTo(isAdjacent);            //         have it path to a spot adjacent to the toil.
+                return false;                             //     Return false.
+            }
+            
+            bool isAdjacent(Path.INode<Block> n) => PointI.SqrDistance((n as Block).Index, Toil.Index) == 1;
         }
     }
 }
