e8d787320544f8e371af47ff602d6e30038dc1a6
JoJoJet <joe102000@gmail.com>
2019-03-02 19:41:32 -0500

    Removed the unused GameTime.DeltaSeconds() extension method

--- Pirates Nueva/Master.cs
+++ Pirates Nueva/Master.cs
@@ -156,11 +156,6 @@ namespace Pirates_Nueva
 
     public static class MasterExt
     {
-        /// <summary>
-        /// Gets the time, in seconds, since last frame.
-        /// </summary>
-        public static float DeltaSeconds(this GameTime gameTime) => (float)gameTime.ElapsedGameTime.TotalSeconds;
-
         /// <summary> Merges every collection of <see cref="T"/>s into a single collection. </summary>
         public static IEnumerable<T> Union<T>(this IEnumerable<IEnumerable<T>> list) {
             foreach(var l in list) {
