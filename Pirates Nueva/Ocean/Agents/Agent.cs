f6db8b667768b09653c604e58289cbdb3ad0e5fc
JoJoJet <joe102000@gmail.com>
2019-03-04 20:36:05 -0500

    Moved Agent containers into their own file

--- Pirates Nueva/Ocean/Agents/Agent.cs
+++ Pirates Nueva/Ocean/Agents/Agent.cs
@@ -1,42 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Pirates_Nueva.Path;
 
 namespace Pirates_Nueva.Ocean.Agents
 {
-    /// <summary>
-    /// An object that can contain Agents and Jobs.
-    /// </summary>
-    /// <typeparam name="TSelf">The type that is implementing this container.</typeparam>
-    /// <typeparam name="TSpot">The type of spot that this instance contains.</typeparam>
-    public interface IAgentContainer<TSelf, TSpot> : IGraph<TSpot>
-        where TSelf : class, IAgentContainer<TSelf, TSpot>
-        where TSpot : class, IAgentSpot<TSpot>
-    {
-        /// <summary>
-        /// Gets a <see cref="Job"/> that can currently be worked on by the specified <see cref="Agent{TC, TSpot}"/>
-        /// </summary>
-        Job<TSelf, TSpot> GetWorkableJob(Agent<TSelf, TSpot> hiree);
-        /// <summary>
-        /// Removes the specified <see cref="Job"/> from this instance.
-        /// </summary>
-        void RemoveJob(Job<TSelf, TSpot> job);
-    }
-    /// <summary>
-    /// An object that an Agent can stand on.
-    /// </summary>
-    /// <typeparam name="T">The type that is implementing this interface.</typeparam>
-    public interface IAgentSpot<T> : INode<T>
-        where T : IAgentSpot<T>
-    {
-        int X { get; }
-        int Y { get; }
-        PointI Index { get; }
-    }
-
     /// <summary>
     /// A character that exists in a container and can complete Jobs.
     /// </summary>
