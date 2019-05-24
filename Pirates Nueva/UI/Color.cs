a2e509c9a05cdc40fc1a5c6baa2a5d194d58f2cb
JoJoJet <joe102000@gmail.com>
2019-05-24 00:01:00 -0400

    Simplify UI.Color

--- Pirates Nueva/UI/Color.cs
+++ Pirates Nueva/UI/Color.cs
@@ -37,17 +37,10 @@ namespace Pirates_Nueva.UI
         /// <param name="g">The green component, 0 to 255.</param>
         /// <param name="b">The blue component, 0 to 255.</param>
         /// <param name="a">The alpha component, 0 to 255.</param>
-        public Color(byte r, byte g, byte b, byte a) {
+        public Color(byte r, byte g, byte b, byte a = 255) {
             R = r;    G = g;
             B = b;    A = a;
         }
-        /// <summary>
-        /// Constructs an RGB color with specified channels, defined with <see cref="byte"/>s.
-        /// </summary>
-        /// <param name="r">The red component, 0 to 255.</param>
-        /// <param name="g">The green component, 0 to 255.</param>
-        /// <param name="b">The blue component, 0 to 255.</param>
-        public Color(byte r, byte g, byte b) : this(r, g, b, 255) {  }
 
         /// <summary>
         /// Constructs an RGBA color with specified channels, defined with <see cref="float"/>s.
@@ -56,31 +49,21 @@ namespace Pirates_Nueva.UI
         /// <param name="g">The green component, 0.0f to 1.0f.</param>
         /// <param name="b">The blue component, 0.0f to 1.0f.</param>
         /// <param name="a">The alpha component, 0.0f to 1.0f.</param>
-        public Color(float r, float g, float b, float a) {
-            R = c(r);
-            G = c(g);
-            B = c(b);
-            A = c(a);
+        public Color(float r, float g, float b, float a = 1f) {
+            R = c(r);    G = c(g);
+            B = c(b);    A = c(a);
 
             byte c(float f) => (byte)((f > 1f ? 1f : (f < 0 ? 0 : f)) * 255);
         }
-        /// <summary>
-        /// Constructs an RGB color with specified channels, defined with <see cref="float"/>s.
-        /// </summary>
-        /// <param name="r">The red component, 0.0f to 1.0f.</param>
-        /// <param name="g">The green component, 0.0f to 1.0f.</param>
-        /// <param name="b">The blue component, 0.0f to 1.0f.</param>
-        public Color(float r, float g, float b) : this(r, g, b, 1f) {  }
 
         public override bool Equals(object obj) {
             switch(obj) {
-                case Color c: return c == this;
-                case MonoColor m: return m == this;
-                case ValueTuple<byte, byte, byte, byte> tb: return tb.Item1 == R && tb.Item2 == G && tb.Item3 == B && tb.Item4 == A;
-                case ValueTuple<float, float, float, float> tf: return tf.Item1 == R && tf.Item2 == G && tf.Item3 == B && tf.Item4 == A;
-                default: return false;
+                case Color c:     return Equals(c);
+                case MonoColor m: return R == m.R && G == m.G && B == m.B && A == m.A;
+                default:          return false;
             }
         }
+        public bool Equals(Color other) => R == other.R && G == other.G && B == other.B && A == other.A;
         public override int GetHashCode() => A.GetHashCode() + G.GetHashCode() * 3 + B.GetHashCode() * 7 + A.GetHashCode() * 14;
 
         public static explicit operator Color(MonoColor mono) => new Color(mono.R, mono.G, mono.B, mono.A);
@@ -88,7 +71,5 @@ namespace Pirates_Nueva.UI
 
         public static bool operator ==(Color a, Color b) => a.R == b.R && a.G == b.G && a.B == b.B && a.A == b.A;
         public static bool operator !=(Color a, Color b) => a.R != b.R || a.G != b.G || a.B != b.B || a.A != b.A;
-
-        bool IEquatable<Color>.Equals(Color other) => R == other.R && G == other.G && B == other.B && A == other.A;
     }
 }
