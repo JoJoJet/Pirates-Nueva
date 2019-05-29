a11e1a1a41c6083978c683d9014c07cf32c09c25
JoJoJet <joe102000@gmail.com>
2019-05-29 16:19:07 -0400

    Add the ItemDef class

---
+++ Pirates Nueva/Defs/ItemDef.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Pirates_Nueva
+{
+    public class ItemDef : Def
+    {
+        public string TextureID { get; protected set; }
+
+        /// <summary>
+        /// Gets the <see cref="ItemDef"/> identified by the specified <see cref="string"/>.
+        /// </summary>
+        /// <exception cref="KeyNotFoundException">Thrown if there is no <see cref="ItemDef"/> identified by /id/.</exception>
+        /// <exception cref="InvalidCastException">Thrown if the <see cref="Def"/> identified by /id/ is not a <see cref="ItemDef"/>.</exception>
+        public static ItemDef Get(string id) => Get<ItemDef>(id);
+
+        protected override void ReadXml(XmlReader reader) {
+            using(var r = reader.ReadSubtree()) {
+                r.ReadToDescendant("TextureID");
+                TextureID = r.ReadElementContentAsString();
+            }
+        }
+    }
+}
