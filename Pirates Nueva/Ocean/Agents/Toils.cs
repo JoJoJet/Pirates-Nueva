333c2dd699f020d0f08e600554cf72c7546752ad
JoJoJet <joe102000@gmail.com>
2019-05-29 22:09:41 -0400

    Added Toils for hauling Stock<> around

--- Pirates Nueva/Ocean/Agents/Toils.cs
+++ Pirates Nueva/Ocean/Agents/Toils.cs
@@ -98,30 +98,206 @@ namespace Pirates_Nueva.Ocean.Agents
     }
 
     /// <summary>
+    /// Has an agent path to some spot.
+    /// </summary>
+    public abstract class PathTo<TC, TSpot> : Job<TC, TSpot>.Action
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected abstract bool IsAtDestination(TSpot spot);
+
+        protected override bool Work(Agent<TC, TSpot> worker, Time delta) {
+            if(worker.PathingTo == null) {                     // If the worker is currently still:
+                if(IsAtDestination(worker.CurrentSpot)) {      //     If its standing next to the toil,
+                    return true;                               //         return true.
+                }                                              //
+                else {                                         //     If its standing away from the toil,
+                    worker.PathTo(IsAtDestination);            //         have it path to a spot adjacent to the toil,
+                    return false;                              //         and return false.
+                }                                              //
+            }                                                  //
+            else {                                             // If the worker is currently pathing:
+                if(IsAtDestination(worker.PathingTo) == false) //     if the worker's destination is not adjacent to the toil,
+                    worker.PathTo(IsAtDestination);            //         have it path to a spot adjacent to the toil.
+                return false;                                  //     Return false.
+            }
+        }
+    }
+    /// <summary>
     /// Has an Agent path to a spot adjacent to the Job or Toil.
     /// </summary>
     /// <typeparam name="TC">The type of Container that this Job exists in.</typeparam>
-    public class PathToAdjacent<TC, TSpot> : Job<TC, TSpot>.Action
+    public class PathToAdjacent<TC, TSpot> : PathTo<TC, TSpot>
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected override bool IsAtDestination(TSpot spot) => PointI.SqrDistance(spot.Index, Toil.Index) == 1;
+    }
+
+
+
+
+    /// <summary>
+    /// Requires that an agent be at the job or toil.
+    /// </summary>
+    public class IsAtToil<TC, TSpot> : Job<TC, TSpot>.Requirement
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected override bool Qualify(Agent<TC, TSpot> worker, out string reason) {
+            if(PointI.SqrDistance(worker.CurrentSpot.Index, (Toil.Index)) == 0) {
+                reason = "";
+                return true;
+            }
+            else {
+                reason = "Worker is not at the job.";
+                return false;
+            }
+        }
+    }
+    /// <summary>
+    /// Requires that an agent be holding a specific type of <see cref="Stock{TC, TSpot}"/>.
+    /// </summary>
+    public class IsHolding<TC, TSpot> : Job<TC, TSpot>.Requirement
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        public ItemDef Holding { get; }
+
+        public IsHolding(ItemDef holding) => Holding = holding;
+
+        protected override bool Qualify(Agent<TC, TSpot> worker, out string reason) {
+            if(worker.Holding?.Def == Holding) {
+                reason = "";
+                return true;
+            }
+            else if(Holding != null) {
+                reason = "Worker is not holding the correct item.";
+                return false;
+            }
+            else {
+                reason = "Worker's hands must be empty.";
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Has an agent place the item that they are holding at their current spot.
+    /// </summary>
+    public class PlaceStockAtAgent<TC, TSpot> : Job<TC, TSpot>.Action
         where TC    : class, IAgentContainer<TC, TSpot>
         where TSpot : class, IAgentSpot<TC, TSpot>
     {
         protected override bool Work(Agent<TC, TSpot> worker, Time delta) {
-            if(worker.PathingTo == null) {                // If the worker is currently still:
-                if(isAdjacent(worker.CurrentSpot)) {      //     If its standing next to the toil,
-                    return true;                          //         return true.
-                }                                         //
-                else {                                    //     If its standing away from the toil,
-                    worker.PathTo(isAdjacent);            //         have it path to a spot adjacent to the toil,
-                    return false;                         //         and return false.
-                }                                         //
-            }                                             //
-            else {                                        // If the worker is currently pathing:
-                if(isAdjacent(worker.PathingTo) == false) //     if the worker's destination is not adjacent to the toil,
-                    worker.PathTo(isAdjacent);            //         have it path to a spot adjacent to the toil.
-                return false;                             //     Return false.
-            }
-            
-            bool isAdjacent(TSpot n) => PointI.SqrDistance(n.Index, Toil.Index) == 1;
+            worker.Holding.Place(worker.CurrentSpot);
+            return true;
         }
+    }
+
+
+    /// <summary>
+    /// Requires that an agent be able to path to the Job or Toil.
+    /// </summary>
+    public class IsAccessible<TC, TSpot> : Job<TC, TSpot>.Requirement
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected override bool Qualify(Agent<TC, TSpot> worker, out string reason) {
+            if(worker.IsAccessible(sp => sp.Index == Toil.Index)) {
+                reason = "";
+                return true;
+            }
+            else {
+                reason = "Worker can't path to the job.";
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Has an agent path to the Job or Toil.
+    /// </summary>
+    public class PathToToil<TC, TSpot> : PathTo<TC, TSpot>
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected override bool IsAtDestination(TSpot spot) => PointI.SqrDistance(spot.Index, Toil.Index) == 0;
+    }
+    
+
+    /// <summary>
+    /// Requires that an Agent be standing on a Stock of specified type.
+    /// </summary>
+    public class IsStandingAtStock<TC, TSpot> : Job<TC, TSpot>.Requirement
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        public ItemDef StockType { get; }
+
+        public IsStandingAtStock(ItemDef stockType) => StockType = stockType;
+
+        protected override bool Qualify(Agent<TC, TSpot> worker, out string reason) {
+            if(worker.CurrentSpot.Stock?.Def == StockType) {
+                reason = "";
+                return true;
+            }
+            else {
+                reason = "Worker is not standing at the correct item.";
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Has an Agent pick up the Stock that they are standing on.
+    /// </summary>
+    public class PickUpStock<TC, TSpot> : Job<TC, TSpot>.Action
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        protected override bool Work(Agent<TC, TSpot> worker, Time delta) {
+            worker.CurrentSpot.Stock.PickUp(worker);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Requires that a specific type of Stock be accessible to an Agent.
+    /// </summary>
+    public class IsStockAcesible<TC, TSpot> : Job<TC, TSpot>.Requirement
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        public ItemDef StockType { get; }
+
+        public IsStockAcesible(ItemDef stockType) => StockType = stockType;
+
+        protected override bool Qualify(Agent<TC, TSpot> worker, out string reason) {
+            if(worker.IsAccessible(at)) {
+                reason = "";
+                return true;
+            }
+            else {
+                reason = "Worker cannot path to the correct item.";
+                return false;
+            }
+
+            bool at(TSpot spot) => spot.Stock?.Def == StockType;
+        }
+    }
+
+    /// <summary>
+    /// Has an Agent path to a specific type of Stock.
+    /// </summary>
+    public class PathToStock<TC, TSpot> : PathTo<TC, TSpot>
+        where TC    : class, IAgentContainer<TC, TSpot>
+        where TSpot : class, IAgentSpot<TC, TSpot>
+    {
+        public ItemDef StockType { get; }
+
+        public PathToStock(ItemDef stockType) => StockType = stockType;
+
+        protected override bool IsAtDestination(TSpot spot) => spot.Stock?.Def == StockType;
     }
 }
