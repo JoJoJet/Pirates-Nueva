c3a61f7789d50187e6470832dd450654cfe64cc6
JoJoJet <joe102000@gmail.com>
2019-05-30 00:59:57 -0400

    Add the Cannon subclass of Furniture

---
+++ Pirates Nueva/Ocean/Ships/Cannon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pirates_Nueva.Ocean.Agents;
+
+namespace Pirates_Nueva.Ocean
+{
+    using Toil = Job<Ship, Block>.Toil;
+    using Requirement = Job<Ship, Block>.Requirement;
+    public class Cannon : Furniture
+    {
+        private Job<Ship, Block> haulJob;
+
+        public ItemDef FuelType { get; } = ItemDef.Get("gunpowder");
+
+        public Cannon(FurnitureDef def, Block floor, Dir direction) : base(def, floor, direction) {  }
+    }
+}
