5fffc508dfbf11f4198e2c5896214aed23c932f8
JoJoJet <joe102000@gmail.com>
2019-06-08 18:58:33 -0400

    Move CannonDef into a separate file

--- Pirates Nueva/Defs/FurnitureDef.cs
+++ Pirates Nueva/Defs/FurnitureDef.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Xml;
+﻿using System.Xml;
 using Pirates_Nueva.Ocean;
 
 namespace Pirates_Nueva
@@ -69,49 +67,5 @@ namespace Pirates_Nueva
         private FurnitureDefImplementation(XmlReader reader) : base(ref reader) { }
 
         public override Furniture Construct(Block floor, Dir direction) => new Furniture(this, floor, direction);
-    }
-
-    /// <summary>
-    /// An immutable object containing properties for a <see cref="Cannon"/> on a <see cref="Ship"/>.
-    /// </summary>
-    public class CannonDef : FurnitureDef
-    {
-        /// <summary>
-        /// The ID of the <see cref="ItemDef"/> that fuels a
-        /// <see cref="Cannon"/> using this <see cref="CannonDef"/>.
-        /// </summary>
-        public string FuelTypeID { get; }
-        /// <summary>
-        /// The name for the current type of <see cref="CannonDef"/>, for use in XML definitions.
-        /// <para /> Should behave like a static property.
-        /// </summary>
-        protected override string TypeName => "CannonDef";
-
-        /// <summary>
-        /// Constructs a new instance of the current type of <see cref="CannonDef"/>,
-        /// reading from the specified <see cref="XmlReader"/>.
-        /// <para /> Should behave like a static method.
-        /// </summary>
-        protected override FurnitureDef Construct(XmlReader reader) => new CannonDef(ref reader);
-        /// <summary>
-        /// Reads the ID attribute, and consumes the TextureID, TextureSize, TextureOrigin, and FuelTypeID nodes.
-        /// </summary>
-        /// <param name="closeReader">
-        /// Whether or not the <see cref="XmlReader"/> should be closed after being read from.
-        /// If this value is `false`, then the reader should be closed by this constructor's caller.
-        /// </param>
-        protected CannonDef(ref XmlReader reader, bool closeReader = true) : base(ref reader, closeReader: false) {
-            reader.ReadToNextSibling("FuelTypeID");
-            FuelTypeID = reader.ReadElementContentAsString();
-            //
-            // Close the reader if we're instructed to.
-            if(closeReader)
-                reader.Dispose();
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="Cannon"/> object using this <see cref="CannonDef"/>.
-        /// </summary>
-        public override Furniture Construct(Block floor, Dir direction) => new Cannon(this, floor, direction);
     }
 }
