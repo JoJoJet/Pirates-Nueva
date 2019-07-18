91ce0604745d87f419ba3ae34d9a6eb2909cc58d
JoJoJet <joe102000@gmail.com>
2019-07-18 17:04:51 -0400

    Add the Ocean.Cannonball class

---
+++ Pirates Nueva/Ocean/Cannonball.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirates_Nueva.Ocean
+{
+    public sealed class Cannonball : Entity, IUpdatable, IDrawable<Sea>
+    {
+        public Sea Sea { get; }
+
+        public float CenterX { get; private set; }
+        public float CenterY { get; private set; }
+        public PointF Center {
+            get => (CenterX, CenterY);
+            private set => (CenterX, CenterY) = value;
+        }
+
+        public Vector Velocity { get; private set; }
+
+        public Cannonball(Sea sea, float centerX, float centerY, Vector velocity) {
+            Sea = sea;
+            (CenterX, CenterY) = (centerX, centerY);
+            Velocity = velocity;
+        }
+
+        protected override BoundingBox GetBounds()
+            => new BoundingBox(CenterX - 0.5f, CenterY - 0.5f, CenterX + 0.5f, CenterY + 0.5f);
+        protected override bool IsCollidingPrecise(PointF point) => true;
+
+        void IUpdatable.Update(Master master, Time delta) {
+            Center += Velocity * delta * 10;
+        }
+
+        void IDrawable<Sea>.Draw(ILocalDrawer<Sea> drawer) {
+            var sprite = Resources.LoadSprite("cannonball");
+            drawer.DrawCenter(sprite, CenterX, CenterY, 1, 1);
+        }
+    }
+}
