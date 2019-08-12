9f489a61b0a0b338293c9a4475c70741fd98deea
JoJoJet <joe102000@gmail.com>
2019-08-12 18:01:07 -0400

    Add the Faction class

---
+++ Pirates Nueva/Ocean/Faction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirates_Nueva.Ocean
+{
+    public sealed class Faction
+    {
+        /// <summary> Whether or not this is the player's <see cref="Faction"/>. </summary>
+        public bool IsPlayer { get; }
+
+        internal Faction(bool isPlayer) => IsPlayer = isPlayer;
+    }
+}
