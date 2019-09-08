a6db0380491f788f99fa5868f61e9f49b651bd44
JoJoJet <joe102000@gmail.com>
2019-09-08 13:42:09 -0400

    Add the IDrawer interface, for an object that can draw things onscreen

--- Pirates Nueva/Controllers/Renderer.cs
+++ Pirates Nueva/Controllers/Renderer.cs
@@ -5,6 +5,47 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Pirates_Nueva
 {
     /// <summary>
+    /// An object that draws things to the screen.
+    /// </summary>
+    public interface IDrawer
+    {
+        /// <summary>
+        /// Submits a <see cref="UI.Sprite"/> to be drawn this frame, drawing from the top left corner.
+        /// </summary>
+        /// <param name="left">The local coordinate of the <see cref="UI.Sprite"/>'s left edge.</param>
+        /// <param name="top">The local coordinate of the <see cref="UI.Sprite"/>'s top edge.</param>
+        /// <param name="width">The width of the <see cref="UI.Sprite"/>, in local units.</param>
+        /// <param name="height">The height of the <see cref="UI.Sprite"/>, in local units.</param>
+        void DrawCorner(UI.Sprite sprite, float left, float top, float width, float height, in UI.Color tint);
+        /// <summary>
+        /// Submits a rotated <see cref="UI.Sprite"/> to be drawn this frame.
+        /// </summary>
+        /// <param name="x">The local x coordinate at which to draw the <see cref="UI.Sprite"/>.</param>
+        /// <param name="y">The local y coordinate at which to draw the <see cref="UI.Sprite"/>.</param>
+        /// <param name="width">The width of the <see cref="UI.Sprite"/>, in local units.</param>
+        /// <param name="height">The height of the <see cref="UI.Sprite"/>, in local units.</param>
+        /// <param name="angle">The <see cref="Angle"/> at which to draw the <see cref="UI.Sprite"/>.</param>
+        /// <param name="origin">The point, relative to the <see cref="UI.Sprite"/>,
+        ///                      from which to draw it. Range: [0, 1].</param>
+        void Draw(UI.Sprite sprite, float x, float y, float width, float height,
+                  in Angle angle, in PointF origin, in UI.Color tint);
+
+        /// <summary>
+        /// Draws a line with specified color.
+        /// </summary>
+        /// <param name="start">The local position of the line's starting point.</param>
+        /// <param name="end">The local position of the line's ending point.</param>
+        void DrawLine(PointF start, PointF end, in UI.Color color);
+
+        /// <summary>
+        /// Submits a <see cref="string"/> to be drawn this frame.
+        /// </summary>
+        /// <param name="left">The local coordinate of the <see cref="string"/>'s left edge.</param>
+        /// <param name="top">The local coordinate of the <see cref="string"/>'s top edge.</param>
+        void DrawString(UI.Font font, string text, float left, float top, in UI.Color color);
+    }
+
+    /// <summary>
     /// An object that transforms and draws things to the screen.
     /// </summary>
     /// <typeparam name="T">The type of object around which things will be drawn.</typeparam>
