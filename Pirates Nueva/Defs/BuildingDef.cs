2c624fe806913c4add9b305ade5cdc9d497cb7be
JoJoJet <joe102000@gmail.com>
2020-01-12 16:23:26 -0500

    Add the BuildingDef class, which defines Buildings that can exist in a village

---
+++ Pirates Nueva/Defs/BuildingDef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Pirates_Nueva
+{
+    /// <summary>
+    /// A Def for a <see cref="Ocean.Building"/> in a <see cref="Ocean.Village"/>.
+    /// </summary>
+    public class BuildingDef : Def<BuildingDef>
+    {
+        protected override string TypeName => "BuildingDef";
+        protected sealed override ResourceInfo Resources => new ResourceInfo("buildings", "BuildingDef");
+
+        protected override BuildingDef Construct(XmlReader reader) => new BuildingDef(ref reader);
+        protected BuildingDef(ref XmlReader reader, bool closeReader = true) : base(reader)
+        {
+            reader = reader.ReadSubtree();
+
+            if(closeReader)
+                reader.Dispose();
+        }
+    }
+}
