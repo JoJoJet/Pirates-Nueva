8ad1f4875f51316625ca53fa6ae333056bd3788a
JoJoJet <joe102000@gmail.com>
2020-01-12 15:02:07 -0500

    Add the VillageDef class

---
+++ Pirates Nueva/Defs/VillageDef.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+
+namespace Pirates_Nueva
+{
+    public class VillageDef : Def<VillageDef>
+    {
+        protected override string TypeName => "VillageDef";
+        protected sealed override ResourceInfo Resources => new ResourceInfo("villages", "VillageDefs");
+
+        protected override VillageDef Construct(XmlReader reader) => new VillageDef(ref reader);
+        /// <summary>
+        /// Reads the ID attribute of the <see cref="XmlReader"/>.
+        /// </summary>
+        /// <param name="closeReader">
+        /// Whether or not the <see cref="XmlReader"/> should be closed after being read from.
+        /// If this value is `false`, then the reader should be closed by this constructor's caller.
+        /// </param>
+        protected VillageDef(ref XmlReader reader, bool closeReader = true) : base(reader)
+        {
+            reader = reader.ReadSubtree();
+
+            if(closeReader)
+                reader.Dispose();
+        }
+    }
+}
